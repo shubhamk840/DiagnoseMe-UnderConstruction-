bafb723bcfff1225576c495fb9c07c88fc135269
shahil198 <60881715+shahil198@users.noreply.github.com>
2021-07-29 16:35:33 +0530

    Fully updated mainheading.js

--- medical_frontend/src/MainHeading.js
+++ medical_frontend/src/MainHeading.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React from 'react';
 import './MainHeading.css'
-import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
-import Navbar from './Navbar';
-function MainHeading() {
-    return (
-        <div className="MainHeading">
-            <>
-            <h1><LocalHospitalIcon/> DiagnoseMe
-            <LocalHospitalIcon/></h1>
-            <Navbar/>
-            </>
-        </div>
-    )
-}
+import {
+Nav,
+NavLink,
+Bars,
+NavMenu,
+NavBtn,
+NavBtnLink,
+} from './NavbarElements';
 
-export default MainHeading
+const Mainheading = () => {
+return (
+	<>
+	<Nav>
+		<Bars />
+
+		<NavMenu>
+		<NavLink to='/homepage' activeStyle className="org_name">
+			DiagnoseMe
+		</NavLink>
+		
+        <NavLink to='/datalist' activeStyle>
+			Datalist
+		</NavLink>
+		<NavLink to='/team' activeStyle>
+			Teams
+		</NavLink>
+		<NavLink to='/about' activeStyle>
+			About 
+		</NavLink>
+		<NavLink to='/' activeStyle>
+			Sign Up
+		</NavLink>
+		</NavMenu>
+		<NavBtn>
+		<NavBtnLink to='/Login'>Sign In</NavBtnLink>
+		</NavBtn>
+	</Nav>
+	</>
+);
+};
+
+export default Mainheading;
